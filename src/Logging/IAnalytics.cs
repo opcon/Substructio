9cc3ee93fb14c3291a8425a988202721b568c245
Patrick Yates <frozen.dinosaur@gmail.com>
2016-07-25 09:57:59 +1000

    Added IAnalytics and IErrorReporting classes

---
+++ src/Logging/IAnalytics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substructio.Logging
+{
+    public interface IAnalytics
+    {
+        void TrackApplicationStartup();
+        void TrackApplicationShutdown();
+        void TrackEvent(string eventCategory, string eventAction, string eventSubjectName = "", string eventValue = "");
+        void TrackApplicationView(string relativeURL, string title = "");
+        void SetCustomVariable(int variableID, string variableName, string variableValue, CustomVariableScope variableScope);
+    }
+
+    public class NullAnalytics : IAnalytics
+    {
+        public void SetCustomVariable(int variableID, string variableName, string variableValue, CustomVariableScope variableScope) { }
+
+        public void TrackApplicationShutdown() { }
+
+        public void TrackApplicationStartup() { }
+
+        public void TrackApplicationView(string relativeURL, string title = "") { }
+
+        public void TrackEvent(string eventCategory, string eventAction, string eventSubjectName = "", string eventValue = "") { }
+    }
+
+    public enum CustomVariableScope
+    {
+        ApplicationLaunch,
+        ApplicationView
+    }
+}
---
+++ src/Logging/IErrorReporting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substructio.Logging
+{
+    public interface IErrorReporting
+    {
+        void ReportError(Exception e);
+        void ReportMessage(string message);
+    }
+
+    public class NullErrorReporting : IErrorReporting
+    {
+        public void ReportError(Exception e) { }
+
+        public void ReportMessage(string message) { }
+    }
+}
