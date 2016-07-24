dacd7daa149ec5b59638f74efddcd536e651484c
Patrick Yates <frozen.dinosaur@gmail.com>
2016-07-25 09:56:44 +1000

    Added NullGameSettings implementation

--- src/Core/Settings/IGameSettings.cs
+++ src/Core/Settings/IGameSettings.cs
@@ -1,9 +1,34 @@
-﻿namespace Substructio.Core.Settings
+﻿using System;
+
+namespace Substructio.Core.Settings
 {
     public interface IGameSettings
     {
         object this[string key] { get; set; }
         void Save();
         void Load();
+    }
+
+    public class NullGameSettings : IGameSettings
+    {
+        public object this[string key]
+        {
+            get
+            {
+                return null;
+            }
+
+            set
+            {
+            }
+        }
+
+        public void Load()
+        {
+        }
+
+        public void Save()
+        {
+        }
     }
 }
