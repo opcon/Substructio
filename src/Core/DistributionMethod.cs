9da414166f90561a06853634e0b4a12a7529ff6f
Patrick Yates <frozen.dinosaur@gmail.com>
2017-02-22 23:27:12 +1100

    Add DistributionMethod class
    Determines what method was used to distribute the current installation

---
+++ src/Core/DistributionMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substructio.Core
+{
+    public enum Distribution
+    {
+        Steam,
+        Squirrel,
+        Itch,
+    }
+
+    public static class DistributionMethod
+    {
+        public static Distribution GetDistributionMethod()
+        {
+            if (File.Exists(ServiceLocator.Directories.Locate("Application", Path.Combine("..", "Update.exe"))))
+                return Distribution.Squirrel;
+            else return Distribution.Itch;
+        }
+    }
+}
